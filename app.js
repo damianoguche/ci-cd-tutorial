7d6b7049ebfe9f7893f0fc27c2ec26b7be5af5ac
Damian Oguche <eguchealaldi@yahoo.com>
2026-07-27 18:41:15 +0100

    Modified app.js file

--- app.js
+++ app.js
@@ -11,12 +11,29 @@ app.get("/", (req, res, next) => {
     <html>
       <head>
         <title>Node Js Web Server</title>
-      </head>
-    </html>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+              font-family: 'Poppins', sans-serif;
+              }
+
+        .welcome-text {
+          margin-top: 30px;
+          text-align: center;
+          font-size: 2rem;      /* 32px */
+          font-weight: 600;
+          color: #333;
+        }
+    </style>
+     </head>
     <body>
-     <p style="margin-top: 30px; text-align: center;">
-     Hello world! I'm a Node/Express JS Web Server...
-</p>
+    <p class="welcome-text">
+      Hello world! I'm a Node/Express JS Web Server...
+    </p>
+    
     </body>
     </html>
     `);
